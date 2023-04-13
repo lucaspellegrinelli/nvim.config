10120989cc415d68588238853fbc56091028745a
Lucas Pellegrinelli <lucas.pellegrinelli@hotmail.com>
2023-04-13 18:34:29 -0300

    Adds term search

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,12 +1,21 @@
 local builtin = require('telescope.builtin')
+local themes = require('telescope.themes')
+
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 vim.keymap.set('n', '<leader>ps', function()
     builtin.grep_string({ search = vim.fn.input("Grep > ") })
 end)
 
+vim.keymap.set('n', '<leader>w', function()
+    builtin.current_buffer_fuzzy_find(themes.get_dropdown {
+        winblend = 10,
+        previewer = false,
+    })
+end, { desc = '[/] Fuzzily search in current buffer' })
+
 -- ignore useless files
-require'telescope'.setup({
+require 'telescope'.setup({
     defaults = {
         file_ignore_patterns = {
             "^./target",
