3939bd8dcc77e9593a0890984b984a83e9ff201a
Lucas Pellegrinelli <lucas.pellegrinelli@hotmail.com>
2023-04-18 17:29:24 -0300

    Adds eslint formatting

--- after/plugin/formatting.lua
+++ after/plugin/formatting.lua
@@ -2,8 +2,9 @@
 local null_ls = require('null-ls')
 null_ls.setup({
     sources = {
-        require'null-ls'.builtins.formatting.black,
-        require'null-ls'.builtins.formatting.isort,
-        require'null-ls'.builtins.formatting.rustfmt,
+        require 'null-ls'.builtins.formatting.black,
+        require 'null-ls'.builtins.formatting.isort,
+        require 'null-ls'.builtins.formatting.rustfmt,
+        require 'null-ls'.builtins.formatting.eslint_d,
     }
 })
