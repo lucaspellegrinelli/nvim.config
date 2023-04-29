c3af084b81561e284dc6a0e8b8ec50f2b19e2a9a
Lucas Pellegrinelli <lucas.pellegrinelli@hotmail.com>
2023-04-29 16:23:38 -0300

    Adds new keymap to replace word in file

--- lua/configs/remap.lua
+++ lua/configs/remap.lua
@@ -30,4 +30,4 @@ vim.keymap.set("n", "<leader>Y", [["+Y]])
 vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
 
 -- Batch replace
--- vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+vim.keymap.set("n", "<leader>rw", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
