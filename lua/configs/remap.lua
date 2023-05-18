931325210874081bb3b7f41a72255c14c0f4aa48
Lucas Pellegrinelli <lucas.pellegrinelli@hotmail.com>
2023-05-18 12:12:49 -0300

    Remove unused keybindings

--- lua/configs/remap.lua
+++ lua/configs/remap.lua
@@ -1,12 +1,6 @@
 -- Setting up <leader>
 vim.g.mapleader = " "
 
--- Window movement
-vim.keymap.set("n", "<C-h>", "<C-w>h")
-vim.keymap.set("n", "<C-j>", "<C-w>j")
-vim.keymap.set("n", "<C-k>", "<C-w>k")
-vim.keymap.set("n", "<C-l>", "<C-w>l")
-
 -- Project file structure viewers
 vim.keymap.set("n", "<leader>p", vim.cmd.Ex)
 
@@ -17,10 +11,6 @@ vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 -- Move cursor half page keep cursor in the middle
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
-
--- Move cursor to the top or bottom of the screen
-vim.keymap.set("n", "n", "nzzzv")
-vim.keymap.set("n", "N", "Nzzzv")
 
 -- Copy to system clipboard
 vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
