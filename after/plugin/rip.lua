83f91164ba6e24499d0cc1c9884e5888170f0899
Lucas Pellegrinelli <lucas.pellegrinelli@hotmail.com>
2023-06-26 19:16:14 -0300

    Removed unused code

--- after/plugin/rip.lua
+++ after/plugin/rip.lua
@@ -1,12 +1,4 @@
 local rip = require("rip")
 
--- rip.setup({
---     keybinds = {
---         toggleMark = "x",
---         toggleCollapse = "l",
---         markAllInFile = "a",
---     },
--- })
-
 vim.keymap.set('n', '<leader>rp', rip.replace_in_project, {})
 vim.keymap.set('n', '<leader>rg', rip.replace_in_git, {})
