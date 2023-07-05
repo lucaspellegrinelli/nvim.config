480a3661494f8d5b181cf0e27d1a3d70839c7152
Lucas Pellegrinelli <lucas.pellegrinelli@hotmail.com>
2023-07-04 21:01:11 -0300

    LSP keybindings

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -16,11 +16,12 @@ lsp.set_preferences({
 })
 
 vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end)
-vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end)
-vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end)
-vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end)
-vim.keymap.set("n", "<C-h>", function() vim.lsp.buf.signature_help() end)
 vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end)
+vim.keymap.set("n", "<leader>ca", function() vim.lsp.buf.code_action() end)
+vim.keymap.set("n", "<leader>rr", function() vim.lsp.buf.references() end)
+vim.keymap.set("n", "<leader>rn", function() vim.lsp.buf.rename() end)
+vim.keymap.set("n", "<C-h>", function() vim.lsp.buf.signature_help() end)
+vim.keymap.set("n", "<C-e>", function() vim.diagnostic.open_float() end)
 
 -- (Optional) Configure lua language server for neovim
 lsp.nvim_workspace()
