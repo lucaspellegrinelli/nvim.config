22ca56759c8573bbdf14bdef791dc63d9c14cd51
Lucas Pellegrinelli <lucas.pellegrinelli@hotmail.com>
2023-10-02 14:03:09 -0300

    Changed telescope to always use ctrl p

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,8 +1,14 @@
 local builtin = require('telescope.builtin')
 local themes = require('telescope.themes')
 
-vim.keymap.set('n', '<C-k>', builtin.find_files, {})
-vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+vim.keymap.set('n', '<C-p>', function()
+    if vim.fn.system('git rev-parse --is-inside-work-tree') == 'true' then
+        builtin.git_files()
+    else
+        builtin.find_files()
+    end
+end, {})
+
 vim.keymap.set('n', '<leader>s', builtin.live_grep, {})
 
 vim.keymap.set('n', '<C-_>', function()
