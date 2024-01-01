4a58c9b5e6ee0575e964216b41250a1849b226bb
Lucas Pellegrinelli <lucas.pellegrinelli@hotmail.com>
2024-01-01 19:12:42 -0300

    Optimizing mason loading

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -4,20 +4,18 @@ return {
         "williamboman/mason-lspconfig.nvim",
         "WhoIsSethDaniel/mason-tool-installer.nvim",
     },
-    config = function()
-        local mason = require("mason")
+    opts = {
+        ui = {
+            icons = {
+                package_installed = "✓",
+                package_pending = "➜",
+                package_uninstalled = "✗",
+            },
+        },
+    },
+    build = function()
         local mason_lspconfig = require("mason-lspconfig")
         local mason_tool_installer = require("mason-tool-installer")
-
-        mason.setup({
-            ui = {
-                icons = {
-                    package_installed = "✓",
-                    package_pending = "➜",
-                    package_uninstalled = "✗",
-                },
-            },
-        })
 
         mason_lspconfig.setup({
             ensure_installed = {
