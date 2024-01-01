7b2bed5778e1d6c937af8d74f89549720fb77d80
Lucas Pellegrinelli <lucas.pellegrinelli@hotmail.com>
2024-01-01 18:12:48 -0300

    Making conform config the reccomended way

--- lua/plugins/format.lua
+++ lua/plugins/format.lua
@@ -1,37 +1,39 @@
 return {
     "stevearc/conform.nvim",
-    lazy = true,
-    event = { "BufReadPre", "BufNewFile" },
-    config = function()
-        local conform = require("conform")
-
-        conform.setup({
-            formatters_by_ft = {
-                javascript = { "prettier" },
-                typescript = { "prettier" },
-                javascriptreact = { "prettier" },
-                typescriptreact = { "prettier" },
-                svelte = { "prettier" },
-                css = { "prettier" },
-                html = { "prettier" },
-                json = { "prettier" },
-                yaml = { "prettier" },
-                lua = { "stylua" },
-                python = { "isort", "black" },
-            },
-            format_on_save = {
-                lsp_fallback = true,
-                async = false,
-                timeout_ms = 1000,
-            },
-        })
-
-        vim.keymap.set({ "n", "v" }, "<leader>f", function()
-            conform.format({
-                lsp_fallback = true,
-                async = false,
-                timeout_ms = 1000,
-            })
-        end, { desc = "Format file or range (in visual mode)" })
-    end,
+    event = { "BufWritePre" },
+    cmd = { "ConformInfo" },
+    opts = {
+        formatters_by_ft = {
+            javascript = { "prettier" },
+            typescript = { "prettier" },
+            javascriptreact = { "prettier" },
+            typescriptreact = { "prettier" },
+            svelte = { "prettier" },
+            css = { "prettier" },
+            html = { "prettier" },
+            json = { "prettier" },
+            yaml = { "prettier" },
+            lua = { "stylua" },
+            python = { "isort", "black" },
+        },
+        format_on_save = {
+            lsp_fallback = true,
+            async = true,
+            timeout_ms = 1000,
+        },
+    },
+    keys = {
+        {
+            "<leader>f",
+            function()
+                require("conform").format({
+                    lsp_fallback = true,
+                    async = true,
+                    timeout_ms = 1000,
+                })
+            end,
+            mode = "",
+            desc = "Format buffer",
+        },
+    },
 }
