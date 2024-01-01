02a001af84d19372ce6639f2858a2d61580ce806
Lucas Pellegrinelli <lucas.pellegrinelli@hotmail.com>
2024-01-01 16:46:16 -0300

    Trying out oil.nvim, so no need for this

--- lua/utils.lua
+++ lua/utils.lua
@@ -1,6 +1,3 @@
--- Mark marked files in netrw
-vim.cmd("highlight netrwMarkFile ctermbg=blue guibg=#9b59b6")
-
 -- Keep { and } from modifying the jumplist
 vim.cmd('nnoremap <silent> } :<C-u>execute "keepjumps norm! " . v:count1 . "}"<CR>')
 vim.cmd('nnoremap <silent> { :<C-u>execute "keepjumps norm! " . v:count1 . "{"<CR>')
