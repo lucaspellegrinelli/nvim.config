629b7bf9ca558c250e56da3fe14c862989e9bff9
Lucas Pellegrinelli <lucas.pellegrinelli@hotmail.com>
2024-01-16 02:20:04 +0000

    Testing out lualine

---
+++ lua/plugins/lualine.lua
@@ -0,0 +1,26 @@
+return {
+    "nvim-lualine/lualine.nvim",
+    dependencies = {
+        "nvim-tree/nvim-web-devicons",
+    },
+    config = function()
+        require("lualine").setup({
+            options = {
+                theme = "onedark",
+                section_separators = "",
+                icons_enabled = true,
+            },
+            sections = {
+                lualine_a = { 'mode' },
+                lualine_b = { 'branch', 'diff', 'diagnostics' },
+                lualine_c = { 'filename' },
+                lualine_x = {},
+                lualine_y = { 'progress' },
+                lualine_z = { 'location' }
+            },
+            extensions = {
+                "oil",
+            },
+        })
+    end
+}
