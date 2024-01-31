c83e4b6a39cdf8adba0573c5addd3587bd3161c5
Lucas Pellegrinelli <lucas.pellegrinelli@hotmail.com>
2024-01-30 23:51:07 -0300

    Updated look of luatab / rosepine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -13,8 +13,8 @@ return {
             sections = {
                 lualine_a = { 'mode' },
                 lualine_b = { 'branch' },
-                lualine_c = { 'filename' },
-                lualine_x = {},
+                lualine_c = { },
+                lualine_x = { 'filename' },
                 lualine_y = { 'progress' },
                 lualine_z = { 'location' }
             },
--- lua/plugins/rosepine.lua
+++ lua/plugins/rosepine.lua
@@ -1,6 +1,9 @@
 return {
     "rose-pine/neovim",
     config = function()
+        require("rose-pine").setup({
+            variant = "moon",
+        })
         vim.cmd("colorscheme rose-pine")
     end,
 }
