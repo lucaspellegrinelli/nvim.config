a5d71fd1dd503c4642926ff9cf2153ce1fb603ed
Lucas Pellegrinelli <lucas.pellegrinelli@hotmail.com>
2024-03-16 00:22:38 -0300

    Fixed rip installation

--- lua/plugins/rip.lua
+++ lua/plugins/rip.lua
@@ -1,7 +1,10 @@
 return {
     "lucaspellegrinelli/rip.nvim",
+    dev=true,
+    dir="~/projects/rip.nvim",
     config = function()
         local rip = require("rip")
+        rip.setup()
 
         vim.keymap.set("n", "<leader>rp", rip.replace_in_project, {})
         vim.keymap.set("n", "<leader>rg", rip.replace_in_git, {})
