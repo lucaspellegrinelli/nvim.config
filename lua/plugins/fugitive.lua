c60349e5eb81387037dab2267d10c19aa71b8b1c
Lucas Pellegrinelli <lucas.pellegrinelli@hotmail.com>
2024-04-04 11:59:15 -0300

    Adiciona confirmacao no force push

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -3,7 +3,12 @@ return {
     config = function()
         vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
         vim.keymap.set("n", "<leader>gp", "<cmd>Git -c push.default=current push<CR>")
-        vim.keymap.set("n", "<leader>gfp", "<cmd>Git -c push.default=current push --force<CR>")
+        vim.keymap.set("n", "<leader>gfp", function ()
+            local confirm = vim.fn.input("Are you sure you want to force push? (y/n): ")
+            if confirm == "y" then
+                vim.cmd("Git -c push.default=current push --force")
+            end
+        end)
 
         vim.keymap.set("n", "ge", "<cmd>diffget //2<CR>")
         vim.keymap.set("n", "gi", "<cmd>diffget //3<CR>")
